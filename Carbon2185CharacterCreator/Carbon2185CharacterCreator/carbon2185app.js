d7b00b45e1a203944b7294d9ffcc303a913499af
Shaunz0r <shaunz0r@gmail.com>
2021-01-23 12:29:38 +0000

    character object creation attempt

--- Carbon2185CharacterCreator/Carbon2185CharacterCreator/carbon2185app.js
+++ Carbon2185CharacterCreator/Carbon2185CharacterCreator/carbon2185app.js
@@ -1,6 +1,20 @@
-var origin = document.getElementById("originSelect");
-var characterObject = {
-    origin: document.querySelector("originSelect"), 
-    class: document.querySelector("classSelect"),
-    
+var characterObject;
+window.onload = function () {
+    var charName = document.getElementById("nameInput");
+    var charOrigin = document.getElementById("originSelect");
+    var charSubOrigin = document.getElementById("subOriginSelect");
+    var charClass = document.getElementById("classSelect");
+    var charBackground = document.getElementById("backgroundSelect");
+    var charContractTerms = document.getElementById("contractTermsSelect");
+    characterObject = {
+        name: charName.value,
+        origin: charOrigin.value,
+        subOrigin: charSubOrigin.value,
+        class: charClass.value,
+        background: charBackground.value,
+        contractTerms: charContractTerms.value
+    };
 }
+
+
+
